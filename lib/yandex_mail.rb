21432ae6058f4fa25fb400fde4ec70f33d4fa202
osfx <osfx@yandex.com>
2017-02-10 19:24:06 +0300

    Add all mailbox action

--- lib/yandex_mail.rb
+++ lib/yandex_mail.rb
@@ -1,38 +1,9 @@
 require "httparty"
-
 require "yandex_mail/version"
-require "yandex_mail/client"
-# require "yandex_mail/email"
-# require "yandex_mail/mailbox"
-#
-# require "yandex_mail/model/base"
-# require "yandex_mail/model/email"
-require 'yaml'
-require "httparty"
-# # require 'pry'
-# require "virtus"
+require 'yandex_mail/client'
+
 
 module YandexMail
-  # autoload :Client, 'yandex_mail/client'
-  # class << self
-  #
-  #
-  # # autoload :Email, 'yandex_mail/email'
-  # def initialize
-  #   @mailbox = Email.new
-  # end
-  #
-  #
-  # def email
-  #   YandexMail::Email.new
-  # end
-  #
-  #
-  # def mailbox
-  #   @mailbox || YandexMail::Mailbox.new
-  # end
-  # # def self.email
-  # #   YandexMail::Email.new
-  # #
-  # end
+	autoload :Client, 'yandex_mail/client'
+	autoload :Mailboxes, 'yandex_mail/client/mailboxes'
 end
--- lib/yandex_mail/client/mailboxes.rb
+++ lib/yandex_mail/client/mailboxes.rb
@@ -2,12 +2,33 @@ module YandexMail
 	class Client
 		module Mailboxes
 
-			def mailboxes(options = {})
+			def mailbox
+				self
+			end
+
+			def add(options = {})
+				response = self.class.post("/email/add", { query: options })
+			end
+
+			def edit(options = {})
+				response = self.class.post("/email/edit", { query: options })
+				response.parsed_response
+			end
+
+			def list(options = {})
 				response = self.class.get("/email/list", { query: options })
 				response.parsed_response["accounts"]
 			end
 
+			def remove(options = {})
+				response = self.class.post("/email/del", { query: options })
+				response.parsed_response
+			end
+
+			def messages(options = {})
+				response = self.class.get("/email/counters?", { query: options })
+				response.parsed_response
+			end
 		end
 	end
-
 end
